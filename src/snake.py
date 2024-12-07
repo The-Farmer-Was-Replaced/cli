d2b6ee62f7d3fe2c4ef51d66fcdfdf4d41eeeb77
flekay <flekay@hotmail.de>
2024-12-07 23:42:25 +0100

    snake fix

--- src/snake.py
+++ src/snake.py
@@ -4,7 +4,10 @@ import src.game as game
 class Snake(game.Game):
     def __init__(self, seed=None):
         super().__init__(seed)
-        self.clear()
+        self._length = 0
+        self._snake_body = []
+        self._apple_spawn_pos = self._get_empty_tile()
+        self.grid[self.player_pos[0]][self.player_pos[1]] = '🍎 '
 
     def clear(self):
         score = self._length
@@ -64,19 +67,21 @@ class Snake(game.Game):
             case _:
                 raise ValueError("Invalid move direction.", direction)
 
-        if self.grid[self.player_pos[0]][self.player_pos[1]] == '🍎 ':
-            self._apple_spawn_pos = self._get_empty_tile()
-
         if self.grid[old_pos[0]][old_pos[1]] == '🍎 ':
             self._length += 1
             self._snake_body.append((old_pos[0], old_pos[1]))
             self.grid[old_pos[0]][old_pos[1]] = '░o░'
-            self.grid[self._apple_spawn_pos[0]][self._apple_spawn_pos[1]] = '🍎 '
+            if self._apple_spawn_pos:
+                self.grid[self._apple_spawn_pos[0]][self._apple_spawn_pos[1]] = '🍎 '
         else:
             self._snake_body.append((old_pos[0], old_pos[1]))
             self.grid[old_pos[0]][old_pos[1]] = '░o░'
             self.grid[self._snake_body[0][0]][self._snake_body[0][1]] = '░░░'
             self._snake_body.pop(0)
+
+        if self.grid[self.player_pos[0]][self.player_pos[1]] == '🍎 ':
+            self._apple_spawn_pos = self._get_empty_tile()
+
         return True
 
     def swap(self, direction):
@@ -115,6 +120,8 @@ class Snake(game.Game):
             for y in range(self.world_size):
                 if self.grid[x][y] == '░░░':
                     empty_tiles.append((x, y))
+        if not empty_tiles:
+            return None
         return random.choice(empty_tiles)
 
     def play(self):
